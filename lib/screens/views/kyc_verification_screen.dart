99d5809bc37c83afe78c28cba7752254117dc6af
Joseph Onipede <onipedejoseph@outlook.com>
2026-02-21 15:50:21 +0100

    added

--- lib/screens/views/kyc_verification_screen.dart
+++ lib/screens/views/kyc_verification_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:ppay_mobile/screens/views/kyc_face_verification_screen.dart';
 import 'package:ppay_mobile/screens/widgets/colors.dart';
 import 'package:ppay_mobile/screens/widgets/document_bottom_sheet.dart';
 import 'package:ppay_mobile/screens/widgets/touch_opacity.dart';
@@ -26,7 +27,7 @@ class _KycVerificationScreenState extends State<KycVerificationScreen> {
           child: TouchOpacity(
             onTap: () => Navigator.pop(context),
             child: SizedBox(
-              height: 24.h,
+              height: 24.w,
               width: 24.w,
               child: SvgPicture.asset(
                 'assets/icon/arrow_back.svg',
@@ -72,6 +73,73 @@ class _KycVerificationScreenState extends State<KycVerificationScreen> {
                 ),
               ),
               4.verticalSpace,
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    isScrollControlled: true,
+                    backgroundColor: Colors.transparent,
+                    builder: (context) {
+                      return DocumentBottomSheet();
+                    },
+                  );
+                },
+                child: AbsorbPointer(
+                  child: TextFormField(
+                    readOnly: true,
+                    showCursor: false,
+                    controller: TextEditingController(
+                      text: 'Driver\'s License',
+                    ),
+                    decoration: InputDecoration(
+                      suffixIcon: SizedBox(
+                        height: 13.h,
+                        width: 7.w,
+                        child: SvgPicture.asset(
+                          'assets/icon/arrow_down.svg',
+                          fit: BoxFit.scaleDown,
+                        ),
+                      ),
+                      hintText: 'Select Document',
+                      hintStyle: TextStyle(
+                        fontFamily: 'InstrumentSans',
+                        color: PPaymobileColors.textfiedBorder,
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 12.w,
+                        vertical: 14.h,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.r),
+                        borderSide: BorderSide(
+                          color: PPaymobileColors.lightGrey,
+                          width: 1.w,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.r),
+                        borderSide: BorderSide(
+                          color: PPaymobileColors.lightGrey,
+                          width: 1.w,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              32.verticalSpace,
+              Text(
+                'Enter Passport number',
+                style: TextStyle(
+                  fontFamily: 'InstrumentSans',
+                  color: Colors.black,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              4.verticalSpace,
               Container(
                 height: 54.h,
                 width: double.infinity,
@@ -80,27 +148,7 @@ class _KycVerificationScreenState extends State<KycVerificationScreen> {
                 ),
                 child: TextFormField(
                   decoration: InputDecoration(
-                    suffixIcon: TouchOpacity(
-                      onTap: () {
-                        showModalBottomSheet(
-                          context: context,
-                          isScrollControlled: true,
-                          backgroundColor: Colors.transparent,
-                          builder: (context) {
-                            return DocumentBottomSheet();
-                          },
-                        );
-                      },
-                      child: SizedBox(
-                        height: 13.h,
-                        width: 7.w,
-                        child: SvgPicture.asset(
-                          'assets/icon/arrow_down.svg',
-                          fit: BoxFit.scaleDown,
-                        ),
-                      ),
-                    ),
-                    hintText: 'Select Document',
+                    hintText: 'Enter Number',
                     hintStyle: TextStyle(
                       fontFamily: 'InstrumentSans',
                       color: PPaymobileColors.textfiedBorder,
@@ -128,7 +176,34 @@ class _KycVerificationScreenState extends State<KycVerificationScreen> {
                   ),
                 ),
               ),
-              356.verticalSpace,
+              14.verticalSpace,
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 18.0).r,
+                    child: SvgPicture.asset(
+                      'assets/icon/info.svg',
+                      width: 14.w,
+                      height: 14.h,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  9.horizontalSpace,
+                  Expanded(
+                    child: Text(
+                      'Enter your Driver’s License ID number as displayed on the front of your license card',
+                      style: TextStyle(
+                        decorationColor: PPaymobileColors.highlightTextColor,
+                        fontFamily: 'Montserrat',
+                        color: PPaymobileColors.buttonColor,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14.sp,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              178.verticalSpace,
               TouchOpacity(
                 child: SizedBox(
                   width: double.infinity,
@@ -142,6 +217,39 @@ class _KycVerificationScreenState extends State<KycVerificationScreen> {
                       elevation: 0,
                     ),
                     onPressed: () {},
+                    child: Text(
+                      'Continue',
+                      style: TextStyle(
+                        fontFamily: 'InstrumentSans',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.sp,
+                        color: PPaymobileColors.mainScreenBackground,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              10.verticalSpace,
+              TouchOpacity(
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50.h,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: PPaymobileColors.backgroundColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(42.r),
+                      ),
+                      elevation: 0,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => KycFaceVerificationScreen(),
+                        ),
+                      );
+                    },
                     child: Text(
                       'Continue',
                       style: TextStyle(
