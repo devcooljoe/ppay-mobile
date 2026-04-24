5d4f1c5edcbac2130e70ad7fbc679f0ffb3b3895
Joseph Onipede <onipedejoseph@outlook.com>
2026-04-24 15:48:18 +0100

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ppay_mobile/firebase_options.dart';
 import 'package:ppay_mobile/screens/views/splash_screen.dart';
 import 'package:ppay_mobile/screens/widgets/colors.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
