a041a7646f4aa5b35b7f0d97dca39e2555f4c824
Joseph Onipede <onipedejoseph@outlook.com>
2026-05-01 14:49:48 +0100

    added

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -5,11 +5,6 @@ import 'app_router.gr.dart';
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
-    AutoRoute(page: SplashRoute.page, initial: true),
-    AutoRoute(page: OnboardRoute.page),
-    AutoRoute(page: LoginRoute.page),
-    AutoRoute(page: SignupRoute.page),
-    AutoRoute(page: RouteRoute.page),
-    AutoRoute(page: HomeRoute.page),
+    
   ];
 }
